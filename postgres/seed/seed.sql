a3096aaa656e6d1fcdc3ff6cd8800b3aa1ce4e99
meilinglong <meilingl.usu@gmail.com>
2020-02-07 11:53:42 -0700

    updating profile

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users(name,email,entries,joined) values('tim','tim@gmail.com','5','2020-01-01');
+INSERT into users(name,email,entries,joined,age,pet) values('tim','tim@gmail.com','5','2020-01-01','26','dog');
 INSERT into login(hash,email) values('$2a$10$awNuZNJBKq7woqMW5sh2de4vUR4CLqeC0gDwI1e0jyMIRjlZZxdAq', 'tim@gmail.com');
 
 COMMIT;
