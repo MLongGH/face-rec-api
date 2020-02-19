50ead41c2795c689b37285a059f0e902dcd585da
meilinglong <meilingl.usu@gmail.com>
2020-02-19 15:54:10 -0700

    get it run locally without docker

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const auth = require('./controllers/authorization');
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB
+    host: '127.0.0.1',
+    user: 'meiling',
+    password: '',
+    database: 'face-rec'
   }
 });
 
