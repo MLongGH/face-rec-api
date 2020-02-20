1d55afc23e6439d2e66bedee34863468b61911a4
meilinglong <meilingl.usu@gmail.com>
2020-02-20 09:34:49 -0700

    change database connection and nodemon to push to heroku

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ const auth = require('./controllers/authorization');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'meiling',
-    password: '',
-    database: 'face-rec'
+    connectionString: process.env.DATABASE_URL,
+    ssl:true
   }
 });
 
